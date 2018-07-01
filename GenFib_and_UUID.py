7eca0585efce5f937e22792e7619db94b1719ef6
RihanZer0 <rinat-sonic@mail.ru>
2018-07-02 02:39:19 +0300

    1&2+tests

--- GenFib_and_UUID.py
+++ GenFib_and_UUID.py
@@ -1,4 +1,5 @@
 import random
+import pytest
 
 s=[]
 def fibo(n):
@@ -7,10 +8,21 @@ def fibo(n):
         fib1, fib2 = fib2, fib1 + fib2
         yield fib1
 
-n = int(input("Введите желаемое количество чисел ряда Фибоначчи: "))
+n = 5
+print(list(fibo(n)))
+
+def listik(n):
+	return list(fibo(n))
+	
+	
+def change():
+	global n
+	n=int(input("Введите желаемое количество чисел ряда Фибоначчи: "))
+	listik(fibo(n))
+
 l = fibo(n)
 
-print(list(fibo(n)))
+
 
 for i in range(n):
   print(next(l))
@@ -28,3 +40,11 @@ def not_UUID():
       not_UUID()
     
 y=not_UUID()
+
+@pytest.mark.parametrize("x,expected", [
+    (2,[1, 1]),
+    (3,[1, 1, 2]),
+    (4,[1, 1, 2, 3]),
+    (5,[1, 1, 2, 3, 5])])
+def test_fibo(x, expected):
+    assert listik(x) == expected
